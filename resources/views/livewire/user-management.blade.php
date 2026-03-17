49ad93d03f3f739cc0b0fb0245ecaea395a99028
Msangi99 <doniaparoma99@gmail.com>
2026-03-18 02:30:47 +0300

    sss

--- resources/views/livewire/user-management.blade.php
+++ resources/views/livewire/user-management.blade.php
@@ -129,7 +129,7 @@
              x-transition:leave="transition ease-in duration-200 transform"
              x-transition:leave-start="scale-100 opacity-100 translate-y-0"
              x-transition:leave-end="scale-95 opacity-0 -translate-y-4"
-             class="bg-white w-full max-w-md max-h-[70vh] rounded-[2rem] shadow-2xl overflow-hidden border border-slate-200 flex flex-col">
+             class="bg-white w-full max-w-sm max-h-[70vh] rounded-[1.75rem] shadow-2xl overflow-hidden border border-slate-200 flex flex-col">
             
             <div class="px-8 py-5 border-b border-slate-100 flex items-center justify-between bg-slate-50/30">
                 <div>
@@ -141,7 +141,7 @@
                 </button>
             </div>
 
-            <form wire:submit.prevent="saveUser" class="p-8 space-y-4 overflow-y-auto custom-scrollbar">
+            <form wire:submit.prevent="saveUser" class="p-6 space-y-4 overflow-y-auto custom-scrollbar">
                 <!-- Success Alert -->
                 @if (session()->has('message'))
                     <div class="p-3 bg-emerald-50 border border-emerald-100 text-emerald-700 rounded-xl flex items-center gap-2 text-xs font-bold mb-3">
